7f8c390e45e4ba70aee557bed2abe32e604e24a2
Alejo Ufano <ufanoalejo@gmail.com>
2025-01-14 08:14:59 -0300

    feat: login routes and otp fixed

--- src/views/Dashboard/screens/DashboardLogin/DashboardLogin.jsx
+++ src/views/Dashboard/screens/DashboardLogin/DashboardLogin.jsx
@@ -1,43 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import styles from './DashboardLogin.module.css';
-import Navbar from '../../components/Navbar/Navbar';
-import { useNavigate } from 'react-router-dom';
-import facturaLogo from '../../assets/logo-facturagpt.svg';
-import { useDispatch, useSelector } from 'react-redux';
-import { OTPInput } from '../../components/OtpInput/OtpInput';
-import { ReactComponent as OpenAiLogo } from '../../assets/openai.svg';
-import { LockIcon } from 'lucide-react';
-import { ReactComponent as KeyIcon } from '../../assets/key-icon.svg';
+import React, { useEffect, useState } from "react";
+import styles from "./DashboardLogin.module.css";
+import Navbar from "../../components/Navbar/Navbar";
+import {
+  useLocation,
+  useNavigate,
+  useParams,
+  useRoutes,
+} from "react-router-dom";
+import facturaLogo from "../../assets/logo-facturagpt.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { OTPInput } from "../../components/OtpInput/OtpInput";
+import { ReactComponent as OpenAiLogo } from "../../assets/openai.svg";
+import { LockIcon, Mail } from "lucide-react";
+import { ReactComponent as KeyIcon } from "../../assets/key-icon.svg";
 import {
   createAccount,
   loginToManager,
   verifyOTP,
   sendOTP,
-
 } from "../../../../actions/user";
 import { useTranslation } from "react-i18next";
 import { useAuth0 } from "@auth0/auth0-react";
 import { setUser } from "../../../../slices/emailManagerSlices";
-
+import sentEmail from "../../assets/sentEmail.svg";
+import i18n from "../../../../i18";
 
 const DashboardLogin = () => {
-  const { t } = useTranslation('dahsboardLogin');
+  const { t } = useTranslation("dahsboardLogin");
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  const { user } = useSelector((state) => state.emailManager);
+  const { user } = useSelector((state) => state.user);
+  const location = useLocation();
   const dispatch = useDispatch();
-  const [mode, setMode] = useState('signin');
-  const [email, setEmail] = useState('');
-  const [nombre, setNombre] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const [otp, setOtp] = useState('');
+  const [mode, setMode] = useState("signin");
+  const [email, setEmail] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [otp, setOtp] = useState("");
   const [resendTimer, setResendTimer] = useState(45);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [storedEmail, setStoredEmail] = useState('');
-  const [storedPassword, setStoredPassword] = useState('');
-  const [recaptchaValue, setRecaptchaValue] = useState('');
+  const [error, setError] = useState("");
+  const [storedEmail, setStoredEmail] = useState("");
+  const [storedPassword, setStoredPassword] = useState("");
+  const [recaptchaValue, setRecaptchaValue] = useState("");
   const navigate = useNavigate();
 
   const validatePassword = (password) => {
@@ -66,6 +72,16 @@ const DashboardLogin = () => {
     return true;
   };
 
+  useEffect(() => {
+    console.log("location", location);
+    if (location?.pathname === "/login" && mode !== "signin") setMode("signin");
+    if (location?.pathname === "/register" && mode !== "signup")
+      setMode("signup");
+    if (location?.pathname === "/recover" && mode !== "forgot-password")
+      setMode("forgot-password");
+    if (location?.pathname === "/otp" && mode !== "otp") setMode("otp");
+  }, [location]);
+
   const handlePasswordChange = (e) => {
     const newPassword = e.target.value;
     setPassword(newPassword);
@@ -74,34 +90,13 @@ const DashboardLogin = () => {
 
   useEffect(() => {
     if (isAuthenticated) {
-      navigate('/home');
+      navigate("/home");
     }
   }, [isAuthenticated]);
 
   useEffect(() => {
-    if (user?.email && user?.id && user?.role) {
-      localStorage.setItem('emailManagerAccount', JSON.stringify(user));
-      navigate('/home');
-    }
-  }, [user, navigate]);
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem('emailManagerAccount');
-    if (storedUser) {
-      try {
-        const parsedUser = JSON.parse(storedUser);
-        if (parsedUser?.email && parsedUser?.id && parsedUser?.role) {
-          dispatch(setUser(parsedUser));
-        }
-      } catch (error) {
-        console.error('Error parsing stored user data:', error);
-      }
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
     let timer;
-    if (mode === 'otp' && resendTimer > 0) {
+    if (mode === "otp" && resendTimer > 0) {
       timer = setInterval(() => {
         setResendTimer((prev) => prev - 1);
       }, 1000);
@@ -110,18 +105,18 @@ const DashboardLogin = () => {
   }, [mode, resendTimer]);
 
   const clearStates = () => {
-    setNombre('');
-    setEmail('');
-    setPassword('');
-    setRepeatPassword('');
-    setOtp('');
-    setError('');
-    setStoredEmail('');
-    setStoredPassword('');
+    setNombre("");
+    setEmail("");
+    setPassword("");
+    setRepeatPassword("");
+    setOtp("");
+    setError("");
+    setStoredEmail("");
+    setStoredPassword("");
   };
 
   useEffect(() => {
-    if (mode !== 'otp') {
+    if (mode !== "otp") {
       setStoredEmail(email);
       setStoredPassword(password);
     }
@@ -135,16 +130,25 @@ const DashboardLogin = () => {
       setIsLoading(true);
       dispatch(loginToManager({ email, password }))
         .unwrap()
-        .then(() => {
+        .then((response) => {
+          let accountData = {
+            email,
+            role: response?.role,
+            accessToken: response?.password,
+            id: response?.id,
+          };
+          localStorage.setItem("user", JSON.stringify(accountData));
           clearStates();
-          navigate('/home');
+          if (response.role !== "user") {
+            navigate("/home");
+          } else {
+            navigate("/panel");
+          }
         })
         .catch((error) => {
-
-          setError(error.message || 'Failed to sign in');
+          setError(error.message || "Failed to sign in");
 
           setError(error.message || "Error al iniciar sesión");
-
         })
         .finally(() => {
           setIsLoading(false);
@@ -152,19 +156,20 @@ const DashboardLogin = () => {
     }
   };
 
-  const handleSignup = () => {
+  const handleSignup = async () => {
     if (storedEmail.length > 1 && storedPassword.length > 1) {
       setIsLoading(true);
-      dispatch(sendOTP({ email: storedEmail }))
+      const language = await localStorage.getItem("language");
+      console.log("language", language);
+      dispatch(sendOTP({ email: storedEmail, language }))
         .unwrap()
         .then(() => {
-          setMode('otp');
+          setMode("otp");
+          navigate("/otp");
           setResendTimer(45);
         })
         .catch((error) => {
-
           setError(error.message || "Error al enviar código de verificación");
-
         })
         .finally(() => {
           setIsLoading(false);
@@ -172,11 +177,12 @@ const DashboardLogin = () => {
     }
   };
 
-  const handleVerifyOTP = () => {
-    if (otp.length === 6) {
+  const handleVerifyOTP = (receivedOtp) => {
+    console.log("on handleVerifyOTP", receivedOtp);
+    if (receivedOtp.length === 6) {
       setIsLoading(true);
-      console.log({ email: storedEmail, otp });
-      dispatch(verifyOTP({ email: storedEmail, otp }))
+      console.log({ email: storedEmail, otp: receivedOtp });
+      dispatch(verifyOTP({ email: storedEmail, otp: receivedOtp }))
         .unwrap()
         .then(() => {
           dispatch(
@@ -189,19 +195,20 @@ const DashboardLogin = () => {
             .unwrap()
             .then(() => {
               clearStates();
-              setMode('signin');
+              setMode("signin");
+              navigate("/login");
             })
             .catch((error) => {
-
               setError(error.message || "Error al crear cuenta");
             });
         })
         .catch((error) => {
           setError(error.message || "Error al verificar código");
-
         })
         .finally(() => {
-          setIsLoading(false);
+          setTimeout(() => {
+            setIsLoading(false);
+          }, 1000);
         });
     }
   };
@@ -214,9 +221,7 @@ const DashboardLogin = () => {
         setResendTimer(45);
       })
       .catch((error) => {
-
         setError(error.message || "Error al reenviar código de verificación");
-
       })
       .finally(() => {
         setIsLoading(false);
@@ -235,23 +240,23 @@ const DashboardLogin = () => {
 
   const renderTitle = () => {
     switch (mode) {
-      case 'signup':
-        return t('title1');
-      case 'otp':
-        return t('title2');
+      case "signup":
+        return t("title1");
+      case "otp":
+        return t("title2");
 
-      case 'forgot-password':
-        return t('title3');
+      case "forgot-password":
+        return t("title3");
 
       default:
-        return t('title4');
+        return t("title4");
     }
   };
 
   const renderLogo = () => (
     <div className={styles.logoContainer}>
       <img
-        onClick={() => navigate('/landing')}
+        onClick={() => navigate("/landing")}
         src={facturaLogo}
         alt="FacturaGPT"
         className={styles.logo}
@@ -264,7 +269,7 @@ const DashboardLogin = () => {
 
   const renderForm = () => (
     <form className={styles.form}>
-      {mode === 'signup' && (
+      {mode === "signup" && (
         <label className={styles.label}>
           Nombre Completo*
           <input
@@ -277,65 +282,66 @@ const DashboardLogin = () => {
         </label>
       )}
       <label className={styles.label}>
-        {t('label1')}
+        {t("label1")}
         <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           type="email"
-          placeholder={t('placeholder1')}
+          placeholder={t("placeholder1")}
           className={styles.input}
         />
       </label>
       <label className={styles.label}>
-        {t('label2')}
+        {t("label2")}
         <input
           value={password}
           onChange={handlePasswordChange}
           type="password"
-          placeholder={t('placeholder2')}
+          placeholder={t("placeholder2")}
           className={styles.input}
         />
         <span className={styles.passwordRequirements}>
-          {t('conditionPassword')}
+          {t("conditionPassword")}
         </span>
       </label>
-      {mode === 'signin' && (
+      {mode === "signin" && (
         <div className={styles.forgotPasswordContainer}>
           <div className={styles.rememberMe}>
             <input type="checkbox" />
-            <span>{t('remember')}</span>
+            <span>{t("remember")}</span>
           </div>
           <a
             href="#"
             className={styles.forgotPassword}
             onClick={(e) => {
               e.preventDefault();
-              setMode('forgot-password');
+              setMode("forgot-password");
+              navigate("/recover");
             }}
           >
-            {t('forgot')}
+            {t("forgot")}
           </a>
         </div>
       )}
       <div
-        onClick={() => (mode === 'signin' ? handleSignin() : handleSignup())}
-        className={`${styles.signInButton} ${isLoading ? styles.loading : ''}`}
+        onClick={() => (mode === "signin" ? handleSignin() : handleSignup())}
+        className={`${styles.signInButton} ${isLoading ? styles.loading : ""}`}
       >
         {isLoading
-          ? mode === 'signin'
-            ? 'Signing in...'
-            : 'Signing up...'
-          : mode === 'signin'
-            ? t('buttonRegister1')
-            : t('buttonRegister2')}
+          ? mode === "signin"
+            ? "Signing in..."
+            : "Signing up..."
+          : mode === "signin"
+            ? t("buttonRegister1")
+            : t("buttonRegister2")}
       </div>
-      {mode === 'signin' && (
+      {mode === "signin" && (
         <button
           onClick={() => loginWithRedirect()}
           className={styles.buttonOpenAi}
         >
           <OpenAiLogo />
-          <span>{t('loginIAButton')}</span>
+          <span>{t("loginIAButton")}</span>
         </button>
       )}
     </form>
@@ -346,16 +352,16 @@ const DashboardLogin = () => {
       <div className={styles.forgotPasswordIcon}>
         <KeyIcon />
       </div>
-      <h1 className={styles.forgotPasswordTitle}>{t('title3')}</h1>
-      <p className={styles.forgotPasswordSubtitle}>{t('solution')}</p>
+      <h1 className={styles.forgotPasswordTitle}>{t("title3")}</h1>
+      <p className={styles.forgotPasswordSubtitle}>{t("solution")}</p>
       <form className={styles.form}>
         <label className={styles.label}>
-          {t('label1')}
+          {t("label1")}
           <input
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             type="email"
-            placeholder={t('placeholder1')}
+            placeholder={t("placeholder1")}
             className={styles.input}
           />
         </label>
@@ -368,34 +374,33 @@ const DashboardLogin = () => {
           className={styles.continueButton}
           disabled={isLoading}
         >
-          {isLoading ? t('buttonContinue2') : t('buttonContinue1')}
+          {isLoading ? t("buttonContinue2") : t("buttonContinue1")}
         </button>
       </form>
       <p className={styles.securityNote}>
         <span className={styles.lockIcon}>
           <LockIcon size={14} />
         </span>
-        {t('security')}
+        {t("security")}
       </p>
     </div>
   );
 
-  if (mode === 'otp') {
+  if (mode === "otp") {
     return (
       <div className={styles.container}>
         <Navbar />
         <div className={styles.content}>
           <div className={styles.rightContainer}>
             <div className={styles.mailIconContainer}>
-              {/* <Mail size={32} className={styles.mailIcon} /> */}
-              icon
+              <img src={sentEmail} alt="sentEmail" />
             </div>
             <h1 className={styles.titleOtp}>{renderTitle()}</h1>
             <p className={styles.subtitleOtp}>
               Enviamos un código para que puedas continuar.
             </p>
             <p className={styles.emailDisplay}>{storedEmail}</p>
-            <OTPInput onChange={setOtp} />
+            <OTPInput onChange={setOtp} handleVerifyOTP={handleVerifyOTP} />
             <div className={styles.resendContainer}>
               <span>Didn't get the code? </span>
               {resendTimer > 0 ? (
@@ -411,18 +416,18 @@ const DashboardLogin = () => {
               )}
             </div>
             <div
-              onClick={handleVerifyOTP}
+              onClick={() => handleVerifyOTP(otp)}
               className={`${styles.signInButton} ${
-                isLoading ? styles.loading : ''
+                isLoading ? styles.loading : ""
               }`}
             >
-              {isLoading ? 'Verifying...' : 'Next'}
+              {isLoading ? "Verifying..." : "Next"}
             </div>
             <p className={styles.error}>{error}</p>
             <p className={styles.securityNote}>
               <span className={styles.lockIcon}>
                 <LockIcon />
-              </span>{' '}
+              </span>{" "}
               Tu seguridad nos importa
             </p>
           </div>
@@ -431,7 +436,7 @@ const DashboardLogin = () => {
     );
   }
 
-  if (mode === 'forgot-password') {
+  if (mode === "forgot-password") {
     return (
       <div className={styles.container}>
         <Navbar />
@@ -450,20 +455,23 @@ const DashboardLogin = () => {
         <div className={styles.leftContainer}>{renderLogo()}</div>
         <div className={styles.rightContainer}>
           <h1 className={styles.title}>{renderTitle()}</h1>
-          <p className={styles.subtitle}>{t('subTitle')}</p>
+          <p className={styles.subtitle}>{t("subTitle")}</p>
           {renderForm()}
           {error && <p className={styles.error}>{error}</p>}
           <p className={styles.footerText}>
-            {mode === 'signin' ? t('notAccount1') : t('yesAccount1')}{' '}
-            {t('notAccount2')}{' '}
+            {mode === "signin" ? t("notAccount1") : t("yesAccount1")}{" "}
+            {t("notAccount2")}{" "}
             <a
-              onClick={() => setMode(mode === 'signin' ? 'signup' : 'signin')}
+              onClick={() => {
+                setMode(mode === "signin" ? "signup" : "signin");
+                navigate(mode === "signin" ? "/register" : "/login");
+              }}
               className={styles.signUp}
             >
-              {mode === 'signin' ? t('register') : t('login')}
+              {mode === "signin" ? t("register") : t("login")}
             </a>
           </p>
-          <p className={styles.footer}>© {t('copyright')}</p>
+          <p className={styles.footer}>© {t("copyright")}</p>
         </div>
       </div>
     </div>
