e8cc4b7c13e34fa65e064d99c7a699afe2e2c717
Yeremi74 <yyeremi15@gmail.com>
2025-01-24 20:58:02 -0400

    Fix: styles docs

--- src/views/Dashboard/components/Button/Button.jsx
+++ src/views/Dashboard/components/Button/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Button.module.css";
-const Button = ({ children, type = "green", action }) => {
+const Button = ({ children, type = "green", action, headerStyle = {} }) => {
   return (
     <button
       onClick={action}
       type="button"
       className={`${styles.buttonTemplate} ${type == "green" && styles.buttonTemplateGreen} ${type == "white" && styles.buttonTemplateWhite}`}
+      style={headerStyle}
     >
       {children}
     </button>
